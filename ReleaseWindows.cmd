6ed282bbff30eb4fd489bc228d671fd6963fa961
Andrew N. Markin <andrew.n.markin@gmail.com>
2026-08-26 22:07:23 +0300

    Package (installer) building for Windows fixed/adjusted

--- ReleaseWindows.cmd
+++ ReleaseWindows.cmd
@@ -1,10 +1,16 @@
 @echo off
 
+if exist ".env" (
+    for /f "usebackq delims=" %%i in (`type ".env" ^| findstr /v "^#" ^| findstr /v "^$"`) do (
+        set "%%i"
+    )
+)
+
 if exist Release (rmdir /S /Q Release)
 mkdir Release\Build
 cd Release\Build
 
-cmake ..\..\Sources -G "Visual Studio 17 2022" -A x64
+cmake ..\..\Sources -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH="%QT_PREFIX%"
 cmake --build . --target PACKAGE --config Release --parallel
 
 move *-*.exe ..
